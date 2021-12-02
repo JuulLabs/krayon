6b427bd0d0a4a01af80c94c0b66f1bc5fa51ab73
Cedrick Cooke <development@cedrickc.net>
2021-12-02 09:58:12 -0800

    Remove empty file; clean up documentation

--- scale/src/commonMain/kotlin/Scale.kt
+++ scale/src/commonMain/kotlin/Scale.kt
@@ -3,7 +3,7 @@ package com.juul.krayon.scale
 /**
  * Maps values from a [D]omain to a [R]ange.
  *
- * Generally, this is used to map from a raw data format to a visual representation, like a color or position location.
+ * Generally, this is used to map from a raw data format (a time, a measurement, etc) to a visual representation (a color, a coordinate, etc).
  */
 public interface Scale<D, R> {
     public fun scale(input: D): R
