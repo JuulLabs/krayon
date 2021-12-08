d2b6d9169d107c4b2496112ad99a4e10ba6c92cd
Travis Wyatt <travis@juul.com>
2021-12-08 10:29:54 -0800

    Use Gradle version catalogs (#40)

--- interpolate/build.gradle.kts
+++ interpolate/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 apply(from = rootProject.file("gradle/jacoco.gradle.kts"))
 
 jacoco {
-    toolVersion = "0.8.7"
+    toolVersion = libs.versions.jacoco.get()
 }
 
 kotlin {
@@ -22,13 +22,13 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 api(project(":kanvas"))
-                api(kotlinx.datetime())
+                api(libs.kotlinx.datetime)
             }
         }
 
         val commonTest by getting {
             dependencies {
-                implementation(tuulbox.test())
+                implementation(libs.tuulbox.test)
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
             }
