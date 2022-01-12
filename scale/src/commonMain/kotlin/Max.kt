a3776a880c24d9d44c8012a358edf6c6855c6ab9
Travis Wyatt <travis@juul.com>
2022-01-12 10:32:19 -0800

    Add `min` and `max` operators (#69)

---
+++ scale/src/commonMain/kotlin/Max.kt
@@ -0,0 +1,12 @@
+package com.juul.krayon.scale
+
+public inline fun <I, O : Comparable<O>> List<I>.max(
+    crossinline selector: (I) -> O?,
+): O {
+    var max: O? = null
+    for (input in this) {
+        val output = selector(input) ?: continue
+        if (max == null || max < output) max = output
+    }
+    return checkNotNull(max)
+}
---
+++ scale/src/commonMain/kotlin/Min.kt
@@ -0,0 +1,12 @@
+package com.juul.krayon.scale
+
+public inline fun <I, O : Comparable<O>> List<I>.min(
+    crossinline selector: (I) -> O?,
+): O {
+    var min: O? = null
+    for (input in this) {
+        val output = selector(input) ?: continue
+        if (min == null || min > output) min = output
+    }
+    return checkNotNull(min)
+}
