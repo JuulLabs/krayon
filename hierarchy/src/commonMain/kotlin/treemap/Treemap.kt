2251d85ebab942c25d56ae1712ddc3c21c72e158
Cedrick Cooke <development@cedrickc.net>
2022-01-14 13:17:23 -0800

    Constructor properties are much more usable in practice

--- hierarchy/src/commonMain/kotlin/treemap/Treemap.kt
+++ hierarchy/src/commonMain/kotlin/treemap/Treemap.kt
@@ -6,42 +6,16 @@ import com.juul.krayon.hierarchy.eachBefore
 
 public fun <T> Node<T, *>.layoutWith(treemap: Treemap<T>): Node<T, Tile> = treemap.layout(this)
 
-public class Treemap<T> {
-
-    public var tileMethod: TileMethod = Squarify()
-        private set
-
-    public var width: Float = 1f
-        private set
-
-    public var height: Float = 1f
-        private set
-
-    // TODO: Accessors for these
-    public var paddingLeft: (Node<T, Tile>) -> Float = { 0f }
-
-    public var paddingTop: (Node<T, Tile>) -> Float = { 0f }
-        private set
-
-    public var paddingRight: (Node<T, Tile>) -> Float = { 0f }
-        private set
-
-    public var paddingBottom: (Node<T, Tile>) -> Float = { 0f }
-        private set
-
-    public var paddingInner: (Node<T, Tile>) -> Float = { 0f }
-        private set
-
-    public fun tile(method: TileMethod): Treemap<T> {
-        this.tileMethod = method
-        return this
-    }
-
-    public fun size(width: Float = this.width, height: Float = this.height): Treemap<T> {
-        this.width = width
-        this.height = height
-        return this
-    }
+public class Treemap<T>(
+    public var width: Float = 1f,
+    public var height: Float = 1f,
+    public var tileMethod: TileMethod = Squarify(),
+    public var paddingLeft: (Node<T, Tile>) -> Float = { 0f },
+    public var paddingTop: (Node<T, Tile>) -> Float = { 0f },
+    public var paddingRight: (Node<T, Tile>) -> Float = { 0f },
+    public var paddingBottom: (Node<T, Tile>) -> Float = { 0f },
+    public var paddingInner: (Node<T, Tile>) -> Float = { 0f },
+) {
 
     public fun layout(root: Node<T, *>): Node<T, Tile> {
         (root as Node<T, Tile>).layout = tile(0f, 0f, width, height)
