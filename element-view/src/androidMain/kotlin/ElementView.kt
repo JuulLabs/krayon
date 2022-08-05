0a1c65ebc5b32648e18020a19736cfc3e84c984d
Cedrick Cooke <development@cedrickc.net>
2022-08-04 20:19:19 -0700

    Refinements for android XML-friendly ElementView touch events

--- element-view/src/androidMain/kotlin/ElementView.kt
+++ element-view/src/androidMain/kotlin/ElementView.kt
@@ -3,11 +3,17 @@ package com.juul.krayon.element.view
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.MotionEvent
+import android.view.MotionEvent.ACTION_CANCEL
+import android.view.MotionEvent.ACTION_DOWN
 import android.view.MotionEvent.ACTION_HOVER_ENTER
 import android.view.MotionEvent.ACTION_HOVER_EXIT
 import android.view.MotionEvent.ACTION_HOVER_MOVE
+import android.view.MotionEvent.ACTION_MOVE
+import android.view.MotionEvent.ACTION_UP
+import android.view.MotionEvent.TOOL_TYPE_FINGER
 import android.view.View
 import android.graphics.Paint as AndroidPaint
 
@@ -47,15 +53,34 @@ public class ElementView @JvmOverloads constructor(
 
     @SuppressLint("ClickableViewAccessibility") // Can't use recommended `performClick` because we need touch coordinates.
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        if (event.actionMasked == MotionEvent.ACTION_DOWN) {
-            adapter?.onClick(event.x, event.y)
-            return true
+        val bounds = RectF(left.toFloat(), top.toFloat(), right.toFloat(), bottom.toFloat())
+        if (!bounds.contains(event.x, event.y)) {
+            // We're receiving a touch event that's off of our bounds. This means that the user has
+            // dragged until they are no longer on the view.
+            adapter?.onHoverOff()
+            return false
         }
-        return false
+
+        when (event.actionMasked) {
+            ACTION_DOWN, ACTION_MOVE -> {
+                adapter?.onHover(event.x, event.y)
+            }
+            ACTION_UP -> {
+                adapter?.onClick(event.x, event.y)
+                if (event.getToolType(0) == TOOL_TYPE_FINGER) {
+                    // Touch loses hover when click ends, but other input types like mouse don't.
+                    adapter?.onHoverOff()
+                }
+            }
+            ACTION_CANCEL -> {
+                adapter?.onHoverOff()
+            }
+        }
+        return true
     }
 
     override fun onHoverEvent(event: MotionEvent): Boolean {
-        when (event.action) {
+        when (event.actionMasked) {
             ACTION_HOVER_ENTER, ACTION_HOVER_MOVE -> adapter?.onHover(event.x, event.y)
             ACTION_HOVER_EXIT -> adapter?.onHoverOff()
             // Should be unreachable, but if they add a new event in the future we shouldn't consume it.
