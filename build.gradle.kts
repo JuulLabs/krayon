b0fb0612b104da47626856a6582c1bcaba966af5
Travis Wyatt <travis@juul.com>
2023-06-26 09:57:07 -0700

    Remove JetBrains Maven repository (#258)
    We no longer use pre-release Compose versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev") // Compose pre-release versions
     }
 
     tasks.withType<Test>().configureEach {
