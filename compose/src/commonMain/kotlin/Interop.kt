06d73b6cd2702df4a8bdd1c434be95a5398da8dc
Cedrick Cooke <development@cedrickc.net>
2024-12-19 10:49:29 -0800

    Expose compose-color interop

---
+++ compose/src/commonMain/kotlin/Interop.kt
@@ -0,0 +1,8 @@
+package com.juul.krayon.compose
+
+import androidx.compose.ui.graphics.toArgb
+import com.juul.krayon.color.Color
+import androidx.compose.ui.graphics.Color as ComposeColor
+
+public fun Color.toCompose(): ComposeColor = ComposeColor(argb)
+public fun ComposeColor.toKrayon(): Color = Color(toArgb())
