359c538a397127fef71e9ec8a8c369b2188af1df
Cedrick Cooke <development@cedrickc.net>
2025-01-06 10:52:25 -0800

    Kotlinter

--- compose/src/commonMain/kotlin/Interop.kt
+++ compose/src/commonMain/kotlin/Interop.kt
@@ -5,4 +5,5 @@ import com.juul.krayon.color.Color
 import androidx.compose.ui.graphics.Color as ComposeColor
 
 public fun Color.toCompose(): ComposeColor = ComposeColor(argb)
+
 public fun ComposeColor.toKrayon(): Color = Color(toArgb())
