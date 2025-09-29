67071b1d416097953b59f6a89fc2866b64981827
Cedrick Cooke <development@cedrickc.net>
2025-09-29 11:09:41 -0700

    Optimize color parsing

--- color/src/commonMain/kotlin/ToColor.kt
+++ color/src/commonMain/kotlin/ToColor.kt
@@ -1,47 +1,48 @@
 package com.juul.krayon.color
 
-private const val HEX = "[a-f0-9]"
-
-private val RGB = """($HEX)($HEX)($HEX)""".toRegex(RegexOption.IGNORE_CASE)
-private val RGBA = """($HEX)($HEX)($HEX)($HEX)""".toRegex(RegexOption.IGNORE_CASE)
-private val RRGGBB = """($HEX{2})($HEX{2})($HEX{2})""".toRegex(RegexOption.IGNORE_CASE)
-private val RRGGBBAA = """($HEX{2})($HEX{2})($HEX{2})($HEX{2})""".toRegex(RegexOption.IGNORE_CASE)
-
 public fun String.toColor(): Color =
     when {
-        this.startsWith("#") -> parseHexNotation(this)
+        this[0] == '#' -> parseHexNotation(this)
         else -> keywordMap[this.lowercase()]
     } ?: throw IllegalArgumentException("Unknown color: `$this`.")
 
 public fun String.toColorOrNull(): Color? =
     try {
         toColor()
-    } catch (e: IllegalArgumentException) {
+    } catch (_: Exception) {
         null
     }
 
-private fun parseHexNotation(text: String): Color {
-    require(text.startsWith("#")) { "Hex notation must start with a pound sign (#)." }
-    val hexText = text.substring(1)
-    val match = when (hexText.length) {
-        3 -> RGB.matchEntire(hexText)
-        4 -> RGBA.matchEntire(hexText)
-        6 -> RRGGBB.matchEntire(hexText)
-        8 -> RRGGBBAA.matchEntire(hexText)
-        else -> throw IllegalArgumentException("Incorrect length. Hex notation must contain exactly 3, 4, 6, or 8 hexadecimal characters.")
-    } ?: throw IllegalArgumentException("Text is not a hexadecimal string. Each character must match the regex [a-fA-F0-9].")
-    val red = match.groupValues[1]
-    val green = match.groupValues[2]
-    val blue = match.groupValues[3]
-    val alpha = match.groupValues.getOrNull(4) ?: "ff"
-    return Color(parseHexComponent(alpha), parseHexComponent(red), parseHexComponent(green), parseHexComponent(blue))
-}
-
-private fun parseHexComponent(component: String): Int =
-    if (component.length == 1) {
-        // Per MDN, "The three-digit notation (#RGB) is a shorter version of the six-digit form (#RRGGBB)."
-        // Achieve that by just calling this function again with the component repeated.
-        parseHexComponent(component + component)
-    } else {
-        component.toInt(HEX_BASE)
+private fun parseHexNotation(text: String): Color = when (text.length) {
+    4 -> { // #rgb
+        val r = text[1].digitToInt(HEX_BASE)
+        val g = text[2].digitToInt(HEX_BASE)
+        val b = text[3].digitToInt(HEX_BASE)
+        Color(alpha = 0xff, red = (r shl 4) or r, green = (g shl 4) or g, blue = (b shl 4) or b)
     }
+
+    5 -> { // #rgba
+        val r = text[1].digitToInt(HEX_BASE)
+        val g = text[2].digitToInt(HEX_BASE)
+        val b = text[3].digitToInt(HEX_BASE)
+        val a = text[4].digitToInt(HEX_BASE)
+        Color(alpha = (a shl 4) or a, red = (r shl 4) or r, green = (g shl 4) or g, blue = (b shl 4) or b)
+    }
+
+    7 -> { // #rrggbb
+        val r = (text[1].digitToInt(HEX_BASE) shl 4) or text[2].digitToInt(HEX_BASE)
+        val g = (text[3].digitToInt(HEX_BASE) shl 4) or text[4].digitToInt(HEX_BASE)
+        val b = (text[5].digitToInt(HEX_BASE) shl 4) or text[6].digitToInt(HEX_BASE)
+        Color(alpha = 0xff, red = r, green = g, blue = b)
+    }
+
+    9 -> { // #rrggbbaa
+        val r = (text[1].digitToInt(HEX_BASE) shl 4) or text[2].digitToInt(HEX_BASE)
+        val g = (text[3].digitToInt(HEX_BASE) shl 4) or text[4].digitToInt(HEX_BASE)
+        val b = (text[5].digitToInt(HEX_BASE) shl 4) or text[6].digitToInt(HEX_BASE)
+        val a = (text[7].digitToInt(HEX_BASE) shl 4) or text[8].digitToInt(HEX_BASE)
+        Color(alpha = a, red = r, green = g, blue = b)
+    }
+
+    else -> throw IllegalArgumentException("Incorrect length. Hex notation must contain exactly 3, 4, 6, or 8 hexadecimal characters.")
+}
