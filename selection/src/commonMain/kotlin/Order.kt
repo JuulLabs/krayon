72e23aad619a4583efed4723ca41094ea27cd5dc
Cedrick Cooke <development@cedrickc.net>
2025-12-10 15:42:40 -0800

    Make order implementation match D3's, and add tests around that behavior

--- selection/src/commonMain/kotlin/Order.kt
+++ selection/src/commonMain/kotlin/Order.kt
@@ -2,15 +2,29 @@ package com.juul.krayon.selection
 
 import com.juul.krayon.element.Element
 
-/** See analogous [d3 function](https://github.com/d3/d3-selection#selection_order). */
+/** See analogous [d3 function](https://d3js.org/d3-selection/modifying#selection_order). */
 public fun <E : Element, D, S : Selection<E, D>> S.order(): S {
     for (group in groups) {
         val parent = group.parent ?: continue
-        for (node in group.nodes) {
-            if (node != null) {
-                parent.appendChild(parent.removeChild(node))
+        val nodes = group.nodes
+
+        var next: E? = null
+        for (index in nodes.lastIndex downTo 0) {
+            val node = nodes[index] ?: continue
+
+            if (next != null) {
+                val children = parent.children
+                val nodeIndex = children.indexOf(node)
+                val nextIndex = children.indexOf(next)
+
+                if (nodeIndex > nextIndex) {
+                    parent.insertBefore(node, next)
+                }
             }
+
+            next = node
         }
     }
+
     return this
 }
